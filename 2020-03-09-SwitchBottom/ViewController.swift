e05de9d1cce5fe9313bddf9c1f11ffc6f9036539
helloconey <48701450+helloconey@users.noreply.github.com>
2020-03-09 19:23:57 -0700

    Initial Commit

---
+++ 2020-03-09-SwitchBottom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020-03-09-SwitchBottom
+//
+//  Created by Coney Lu on 3/9/20.
+//  Copyright © 2020 Hua Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
